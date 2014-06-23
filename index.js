136d506aa54236a1748adacca4a46e96c7335b1c
Christoph Hermann <schtoeffel@gmail.com>
2014-06-23 14:29:33 +0200

    adds total functionality

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-var PREFIX = ':';
+var moment = require('moment');
 
-module.exports = function(theString, theObject) {
-    theString = (typeof theString === 'undefined')?'': theString;
-    theString = String(theString);
+module.exports = function(theString, theObject, config) {
+  config = config || {};
+  var prefix = config.prefix || ':',
+      dateFormat = config.dateFormat || 'DD.MM.YYYY';
 
-    theObject = theObject || {};
+  theString = (typeof theString === 'undefined') ? '' : theString;
+  theString = String(theString);
 
-    theString = theString.replace(PREFIX + 'prop', theObject.prop);
-    return theString;
+  theObject = theObject || {};
+
+  Object.keys(theObject).forEach(function(key) {
+    var value = theObject[key];
+    if (theObject[key] instanceof Date) {
+      value = moment(theObject[key]).format(dateFormat);
+    }
+    theString = theString.replace(prefix + key, value);
+  });
+  return theString;
 };
--- test.js
+++ test.js
@@ -1,24 +1,55 @@
 var formatString = require('./index.js'),
-    assert = require('assert');
+  assert = require('assert');
 
 describe('format-string', function() {
-    it('should return the initial string', function() {
-        assert.equal(formatString('Hello, world!'), 'Hello, world!');
-    });
+  var obj, day;
+  beforeEach(function() {
+    day = new Date(2014, 11, 30);
+    obj = {
+      prop: 'value',
+      another: 42,
+      ohNoADate: day
+    };
+  });
 
-    it('should return an empty string if none given', function() {
-        assert.equal(formatString(), '');
-    });
+  it('should return the initial string', function() {
+    assert.equal(formatString('Hello, world!'), 'Hello, world!');
+  });
 
-    it('should return a string if none given', function() {
-        assert.equal(formatString(10), '10');
-        assert.equal(formatString(0), '0');
-    });
+  it('should return an empty string if none given', function() {
+    assert.equal(formatString(), '');
+  });
 
-    it('should replace a prefixed property with its value', function() {
-        var obj = {
-            prop: 'value'
-        };
-        assert.equal(formatString(':prop', obj), 'value');
-    });
+  it('should return a string if none given', function() {
+    assert.equal(formatString(10), '10');
+    assert.equal(formatString(0), '0');
+  });
+
+  it('should replace a prefixed property with its value', function() {
+    assert.equal(formatString(':prop', obj), 'value');
+    assert.equal(formatString(':another', obj), '42');
+    assert.equal(formatString(':prop = :another', obj), 'value = 42');
+    assert.equal(formatString(':prop:another', obj), 'value42');
+    assert.equal(formatString(':property', obj), 'valueerty');
+  });
+
+  it('should replace dates with a formated date', function() {
+    assert.equal(formatString(':ohNoADate', obj), '30.12.2014');
+  });
+
+  it('should formate dates using a given format', function() {
+    assert.equal(formatString(':ohNoADate', obj, {
+      dateFormat: 'DD-MM-YY'
+    }), '30-12-14');
+  });
+
+  it('shouldn\'t replace a prefixed property if the object doesn\'t own it', function() {
+    assert.equal(formatString(':nope', obj), ':nope');
+  });
+
+  it('should be possible to change the prefix', function() {
+    assert.equal(formatString('$prop', obj, {
+      prefix: '$'
+    }), 'value');
+  });
 });
